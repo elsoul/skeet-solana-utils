8e207543efd135854d49a890f1c5528752b4bb55
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2023-10-16 13:54:28 +0200

    update: release v0.1.2

--- dist/lib/solanaUtils/index.d.ts
+++ dist/lib/solanaUtils/index.d.ts
@@ -2,10 +2,11 @@ import { Connection } from '@solana/web3.js';
 import { SolanaUtilsOptions } from './solanaUtilsTypes';
 export declare class SolanaUtils {
     connection: Connection;
+    options: SolanaUtilsOptions;
     constructor(options: SolanaUtilsOptions);
     getStakeAccountBalance(this: SolanaUtils, stakeAccountAddress: string): Promise<number>;
     getLastStakingReward(this: SolanaUtils, stakeAccountAddress: string): Promise<import("./solanaUtilsTypes").StakeRewardParams | null>;
-    getOwnerNFTs(this: SolanaUtils, userWalletAddress: string): Promise<import("@metaplex-foundation/js").FindNftsByOwnerOutput>;
+    getOwnerNFTs(this: SolanaUtils, userWalletAddress: string): Promise<import("@metaplex-foundation/mpl-token-metadata").DigitalAsset[]>;
     checkNFTsOwner(this: SolanaUtils, userWalletAddress: string, collectionAddress: string, nftAddresses: string[]): Promise<{
         status: boolean;
         message: string;
--- dist/lib/solanaUtils/nftOwnerAddress.d.ts
+++ dist/lib/solanaUtils/nftOwnerAddress.d.ts
@@ -1,5 +1,5 @@
 import { SolanaUtils } from '.';
-export declare const nftOwnerAddress: (solanaUtils: SolanaUtils, userWalletAddress: string) => Promise<import("@metaplex-foundation/js").FindNftsByOwnerOutput>;
+export declare const nftOwnerAddress: (solanaUtils: SolanaUtils, userWalletAddress: string) => Promise<import("@metaplex-foundation/mpl-token-metadata").DigitalAsset[]>;
 export declare const nftCollectionOwnerAddress: (solanaUtils: SolanaUtils, userWalletAddress: string, collectionAddress: string) => Promise<string[]>;
 export declare const checkOwnerNFTs: (solanaUtils: SolanaUtils, userWalletAddress: string, collectionAddress: string, nftAddresses: string[]) => Promise<{
     status: boolean;
