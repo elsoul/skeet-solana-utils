2228238c99d74631a40a354f330f2622a764ab57
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2023-10-16 18:15:31 +0200

    update: release v0.1.3

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,1 +1,5 @@
 export { SolanaUtils } from '@/lib/solanaUtils';
+export type { SolanaUtilsOptions } from '@/lib/solanaUtils/solanaUtilsTypes';
+export { stakeAccountBalance } from '@/lib/solanaUtils/stakeAccountBalance';
+export { lastStakingReward } from '@/lib/solanaUtils/lastStakingReward';
+export { checkOwnerNFTs, nftOwnerAddress, } from '@/lib/solanaUtils/nftOwnerAddress';
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
+// index.ts
 export { SolanaUtils } from '@/lib/solanaUtils'
+export type { SolanaUtilsOptions } from '@/lib/solanaUtils/solanaUtilsTypes'
+export { stakeAccountBalance } from '@/lib/solanaUtils/stakeAccountBalance'
+export { lastStakingReward } from '@/lib/solanaUtils/lastStakingReward'
+export {
+  checkOwnerNFTs,
+  nftOwnerAddress,
+} from '@/lib/solanaUtils/nftOwnerAddress'
